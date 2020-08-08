45d6df4d0aed8e4b9458766a5e2bf6831ea0adb0
hs8y <git@hs8y.jp>
2020-08-08 23:27:42 +0900

    update footer contents

--- src/js/components/AppFooter.vue
+++ src/js/components/AppFooter.vue
@@ -3,10 +3,19 @@
     <b-container>
       <div class="app_footer__body">
 
-        <div class="">
-          <a href="https://pronama.jp/2020/07/31/petitcon-2020-summer/" target="_blank">
-            #プロ生ちゃんプロコン <i class="fas fa-external-link-alt"></i>
-          </a>
+        <div class="app_footer__menu">
+          <ul class="app_footer__menu__list">
+            <li class="app_footer__menu__list__item">
+              <a href="https://twitter.com/hs8yjp" target="_blank">
+                お問い合わせ <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="app_footer__menu__list__item">
+              <a href="https://pronama.jp/2020/07/31/petitcon-2020-summer/" target="_blank">
+                #プロ生ちゃんプロコン <i class="fas fa-external-link-alt"></i>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
       <div class="app_footer__copyright">
